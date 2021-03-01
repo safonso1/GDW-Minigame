e7f1db42c08ea754513cb75d7bbbb12f1d345e02
safonso1 <safonso@student.fitchburgstate.edu>
2021-03-01 13:33:22 -0500

    Added Minigame Project

---
+++ Assets/Scripts/DestoryOutOfBounds.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -25;
+    private float sideBound = -25;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+        else if (transform.position.x < sideBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = -20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, 1), 1.5f, spawnPosZ);
+
+        Instantiate(ballPrefabs[animalIndex], spawnPos, ballPrefabs[animalIndex].transform.rotation);
+    }
+}
